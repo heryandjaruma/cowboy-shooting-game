aa653146ae10f1b416ca003d2078075faa729d46
RyanDJ <heryandjaruma@gmail.com>
2026-06-24 16:03:07 +0800

    Initial Commit

---
+++ Cowboy Shooting Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cowboy Shooting Game
+//
+//  Created by Heryan Djaruma on 24/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cowboy Shooting Game/Cowboy_Shooting_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cowboy_Shooting_GameApp.swift
+//  Cowboy Shooting Game
+//
+//  Created by Heryan Djaruma on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Cowboy_Shooting_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
