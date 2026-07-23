61af3dda2214ab1f41eb92764e349a6693fa8fe6
Cello Farrel Tanojo <109634862+cellofarrel@users.noreply.github.com>
2026-07-23 16:14:21 +0800

    works real

---
+++ Cowboy Shooting Game/Views/PauseMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  PauseMenuView.swift
+//  Cowboy Shooting Game
+//
+//  Created by Rayne on 23/07/26.
+//
+
+import SwiftUI
+
+struct PauseMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PauseMenuView()
+}
