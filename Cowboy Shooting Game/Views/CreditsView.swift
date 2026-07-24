95eda03fb3b9d11185d1a1a15992ababe51dde62
RyanDJ <heryandjaruma@gmail.com>
2026-07-24 15:56:10 +0800

    add credits

--- Cowboy Shooting Game/Views/CreditsView.swift
+++ Cowboy Shooting Game/Views/CreditsView.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 struct CreditsView: View {
     @Environment(\.dismiss) private var dismiss
-
+    
     private struct Credit: Identifiable {
         let id = UUID() // Just in case we wanna do the role role thingies
         let role: LocalizedStringResource
         let people: String
     }
-
+    
     private let credits: [Credit] = [
         Credit(role: "Project Manager", people: "Ryan Firdaus"),
         Credit(role: "Technical Lead", people: "Heryan Djaruma"),
@@ -17,7 +17,7 @@ struct CreditsView: View {
         Credit(role: "UI Designers", people: "Annisa Rahmadani, Cello Tanojo"),
         Credit(role: "Music & SFX", people: "Heryan Djaruma")
     ]
-
+    
     var body: some View {
         ZStack(alignment: .top) {
             Image(.backgroundMainScreen)
@@ -27,24 +27,41 @@ struct CreditsView: View {
             Rectangle()
                 .fill(Color.black.opacity(0.5))
                 .ignoresSafeArea(edges: .all)
-
+            
             VStack {
                 ScreenTopBar(title: "Credits") {
                     dismiss()
                 }
-
-                VStack(alignment: .leading, spacing: 12) {
-                    ForEach(credits) { credit in
-                        Text(
-                            "\(Text(credit.role).foregroundStyle(Color.ternaryCSG)): \(Text(credit.people).foregroundStyle(Color.ternaryCSG.opacity(0.7)))"
-                        )
-                            .font(.bodyCSG)
-                            .multilineTextAlignment(.leading)
-                            .fixedSize(horizontal: false, vertical: true)
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ScrollView(.vertical, showsIndicators: true) {
+                    Grid {
+                        GridRow {
+                            Image("ian")
+                                .resizable()
+                                .scaledToFit()
+                            Image("ryan")
+                                .resizable()
+                                .scaledToFit()
+                            Image("cello")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        GridRow {
+                            Image("nisa")
+                                .resizable()
+                                .scaledToFit()
+                            Image("reward")
+                                .resizable()
+                                .scaledToFit()
+                            Image("max")
+                                .resizable()
+                                .scaledToFit()
+                        }
                     }
+                    .frame(maxWidth: .infinity)
                 }
                 .padding(20)
+                .frame(maxWidth: .infinity)
                 .background(
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color.primaryCSG)
@@ -55,7 +72,7 @@ struct CreditsView: View {
                 )
                 .padding(.top, 20)
                 .padding(.horizontal, 16)
-
+                
                 Spacer()
             }
             .padding(.top, 20)
